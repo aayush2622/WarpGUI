20bd17e5ca3ca5fcbac8cc02eb07d3fc0b85ade2
aayush262 <aayushthakur262006@gmail.com>
2025-08-24 21:58:01 +0530

    Initial commit

---
+++ src/main/kotlin/mc/aayush262/WarpGUI.kt
@@ -0,0 +1,14 @@
+package me.aayush262
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WarpGUI : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
